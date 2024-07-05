00b98fd0aaa91d5d184610320d6f5e9c31033799
Sunteang <129640164+Sunteang@users.noreply.github.com>
2024-07-05 16:21:54 +0700

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -128,11 +128,7 @@ function factorial(num){
     }else if (num===0 || num===1){
         return 1;
     }else{
-        let result = 1;
-        for (let i = 2 ; i <= num ; i++){
-            result *= i;
-        }
-        return result;
+        return num * factorial(num - 1);
     }
 }
 
